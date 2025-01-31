934ebe90a8ebf9d2134d830d7ec639a2dc889a9d
Sadykov Arsen <LoCo10291998@gmail.com>
2025-01-31 18:34:56 +0600

    Commit. Added a little improvement to the component to display a list of all countries.

--- src/components/ArrayOfCountries/ArrayOfCountries.tsx
+++ src/components/ArrayOfCountries/ArrayOfCountries.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Countries} from "../../types";
+import { Countries } from "../../types";
 
 interface Props {
     countries: Countries[];
@@ -7,33 +7,40 @@ interface Props {
     onClickedCountry: (code: string) => void;
 }
 
-
 const ArrayOfCountries: React.FC<Props> = ({ countries, onClicked, onClickedCountry }) => {
     return (
         <div className="col-md-4">
             <h3>Список стран</h3>
-            <ul className="list-group">
-                {countries.map((country) => {
-                    let className = "list-group-item";
-                    if (onClicked === country.alpha3Code) {
-                        className += " active";
-                    }
+            <div
+                className="list-group-container"
+                style={{
+                    maxHeight: "600px",
+                    overflowY: "auto",
+                    border: "1px solid #ccc",
+                    borderRadius: "5px",
+                }}
+            >
+                <ul className="list-group" style={{ padding: 0 }}>
+                    {countries.map((country) => {
+                        let className = "list-group-item";
+                        if (onClicked === country.alpha3Code) {
+                            className += " active";
+                        }
 
-                    return (
-                        <li
-                            key={country.alpha3Code}
-                            className={className}
-                            onClick={() => onClickedCountry(country.alpha3Code)}
-                            style={{ cursor: "pointer" }}
-                        >
-                            {country.name}
-                        </li>
-                    );
-                })}
-            </ul>
+                        return (
+                            <li
+                                key={country.alpha3Code}
+                                className={className}
+                                onClick={() => onClickedCountry(country.alpha3Code)}
+                            >
+                                {country.name}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
     );
 };
-
 
 export default ArrayOfCountries;
